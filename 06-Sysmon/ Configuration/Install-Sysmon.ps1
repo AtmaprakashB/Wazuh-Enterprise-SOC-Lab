b9ea420b4054904ad43ca9b14e32033b0edb9fa2
Atmaprakash Balawat <58217834+AtmaprakashB@users.noreply.github.com>
2026-08-03 22:59:43 +0530

    Update Install-Sysmon.ps1

--- 06-Sysmon/ Configuration/Install-Sysmon.ps1
+++ 06-Sysmon/ Configuration/Install-Sysmon.ps1
@@ -1,1 +1,32 @@
+# =============================================================================
+# Microsoft Hybrid SOC Lab
+# Sysmon Installation Script
+# =============================================================================
 
+$SysmonPath = "C:\Tools\Sysmon\Sysmon64.exe"
+$ConfigPath = "C:\Tools\Sysmon\sysmonconfig.xml"
+
+Write-Host ""
+Write-Host "========================================"
+Write-Host " Installing Microsoft Sysmon"
+Write-Host "========================================"
+Write-Host ""
+
+if (!(Test-Path $SysmonPath)) {
+    Write-Host "[ERROR] Sysmon64.exe not found." -ForegroundColor Red
+    exit
+}
+
+if (!(Test-Path $ConfigPath)) {
+    Write-Host "[ERROR] sysmonconfig.xml not found." -ForegroundColor Red
+    exit
+}
+
+& $SysmonPath -accepteula -i $ConfigPath
+
+Write-Host ""
+Write-Host "Checking Sysmon service..."
+Get-Service Sysmon64
+
+Write-Host ""
+Write-Host "Installation completed."
